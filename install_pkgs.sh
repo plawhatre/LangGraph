968981c314a0e41937f3ddf66f397f65a1fcecf5
prashant <prashantlawhatre@gmail.com>
2025-05-17 16:51:53 +0530

    modified unkown changes

---
+++ install_pkgs.sh
@@ -0,0 +1,13 @@
+# Install the Ollama Python package
+source .venv/bin/activate
+# Install the required Python packages
+python3 -m pip install -r requirements.txt
+python3 -m pip install langchain-community pypdf
+python3 -m pip install faiss-cpu
+# vector db
+pip install --upgrade setuptools
+pip install protobuf==6.30.0
+python3 -m pip install langchain-chroma>=0.1.2
+python3 -m pip install rank_bm25
+python3 -m pip install langgraph>0.2.27
+python3 -m pip install bs4
