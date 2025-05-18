36af3ac4348543eb73954fb49e8082ae1f3a6f13
prashant <prashantlawhatre@gmail.com>
2025-05-18 22:21:27 +0530

    modified gitignore

---
+++ 05_chatbot_with_customized_state.py
@@ -0,0 +1,9 @@
+from typing import Annotated, TypedDict
+from langgraph.graph import START, END, StateGraph
+from langgraph.graph.message import add_messages
+
+class State(TypedDict)
+
+
+if __name__ == "__main__":
+    pass
