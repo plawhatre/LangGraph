4112f5fe9743a48edd20964fc652620979f7486e
prashant <prashantlawhatre@gmail.com>
2025-05-23 21:06:53 +0530

    added math server for mcp

---
+++ 07_MCP_server_math.py
@@ -0,0 +1,17 @@
+import mcp
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Math")
+
+@mcp.tool
+def add(a: float, b: float) -> float:
+    "Add the given two numbers"
+    return a+b
+
+@mcp.tool
+def multiply(a: float, b: float) -> float:
+    "Multiply the given two numbers"
+    return a*b
+
+if __name__ == "__main":
+    mcp.run(transport="stdio")
