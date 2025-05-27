49e2fe76e4f3c2ea4b014dbf89f7317efeb605e0
prashant <prashantlawhatre@gmail.com>
2025-05-27 22:07:32 +0530

    added code to read from LTM

--- 08b_a1_agent_with_long_term_memory_read.py
+++ 08b_a1_agent_with_long_term_memory_read.py
@@ -1,16 +1,14 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.store.memory import InMemoryStore
 from langgraph.prebuilt import create_react_agent
-from langgraph.prebuilt.chat_agent_executor import AgentState
 from langgraph.config import get_store
 
-class CustomState(AgentState):
-    key: str
 
-def get_user_info(state: CustomState) -> str:
+def get_user_info() -> str:
     """Look up user info from LTM"""
-    store = get_store(("users_namespace",), state["key"])
-    values = store.get()
+    key = input("Enter the Key for Long Term Memory:\n\t")
+    store = get_store()
+    values = store.get(("users_namespace",), key)
     return str(values.value) if values else "unkown user"
 
 if __name__ == "__main__":
@@ -35,9 +33,7 @@ if __name__ == "__main__":
     agent = create_react_agent(
         model=model,
         tools=[get_user_info],
-        store=store,
-        state_schema=CustomState,
-        prompt=prompt
+        store=store
     )
 
     # Step 4: Invoke
@@ -47,8 +43,7 @@ if __name__ == "__main__":
         "content": "Get the user information"
     }]
     resp = agent.invoke({
-        "messages": messages, 
-        "key": "user_key"
+        "messages": messages
     }, config=config)
     # resp["messages"][-1].pretty_print()
     for msg in resp["messages"]:
